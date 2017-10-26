a39a35d1a09f8c0189b40c828ac025af7ec270fd
Dorian Rosen <dorianrrosen@gmail.com>
2017-10-26 10:41:04 -0600

    update while combining

--- Assets/Scripts/Trail/MoveTrailAnchor.cs
+++ Assets/Scripts/Trail/MoveTrailAnchor.cs
@@ -1,10 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using UnityEngine.UI;
 using Trails;
 
 public class MoveTrailAnchor : MonoBehaviour
 {
+    enum color { white, blue, red, yellow, orange, purple, green, brown };
+    public Material[] materials;
+
+    public Renderer rend;
+    public GameObject[] ColorPool;
+
+    private Dictionary<string, int> colorCollected = new Dictionary<string, int>();
+    public Text output;
 
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
@@ -17,7 +27,13 @@ public class MoveTrailAnchor : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        output.text = "";
 
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        colorCollected.Add("blue", 0);
+        colorCollected.Add("red", 0);
+        colorCollected.Add("yellow", 0);
     }
 
     // Update is called once per frame
@@ -25,7 +41,7 @@ public class MoveTrailAnchor : MonoBehaviour
     {
 
         CharacterController controller = GetComponent<CharacterController>();
-        
+
         //Roate Player via keyboard
         //transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
 
@@ -49,5 +65,110 @@ public class MoveTrailAnchor : MonoBehaviour
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("White Pool"))
+        {
+            rend.sharedMaterial = materials[0];
+            for (int i = 0; i < 3; i++)
+            {
+                ColorPool[i].SetActive(true);
+            }
+            colorCollected["blue"] = 0;
+            colorCollected["red"] = 0;
+            colorCollected["yellow"] = 0;
+        }
+        else if (other.gameObject.CompareTag("Blue Pool"))
+        {
+            colorCollected["blue"]++;
+            if (colorCollected["blue"] == 1 && colorCollected["red"] == 0 && colorCollected["yellow"] == 0)
+            {
+                rend.sharedMaterial = materials[(int)color.blue];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 1 && colorCollected["yellow"] == 0)
+            {
+                rend.sharedMaterial = materials[(int)color.purple];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 0 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.green];
+            }
+            else if (colorCollected["blue"] == 0 && colorCollected["red"] == 1 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.orange];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 1 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.brown];
+            }
+
+            other.gameObject.SetActive(false);
+
+        }
+        else if (other.gameObject.CompareTag("Red Pool"))
+        {
+            colorCollected["red"]++;
+            if (colorCollected["blue"] == 0 && colorCollected["red"] == 1 && colorCollected["yellow"] == 0)
+            {
+                rend.sharedMaterial = materials[(int)color.red];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 1 && colorCollected["yellow"] == 0)
+            {
+                rend.sharedMaterial = materials[(int)color.purple];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 0 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.green];
+            }
+            else if (colorCollected["blue"] == 0 && colorCollected["red"] == 1 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.orange];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 1 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.brown];
+            }
+            other.gameObject.SetActive(false);
+
+        }
+        else if (other.gameObject.CompareTag("Yellow Pool"))
+        {
+            colorCollected["yellow"]++;
+            if (colorCollected["blue"] == 0 && colorCollected["red"] == 0 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.yellow];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 1 && colorCollected["yellow"] == 0)
+            {
+                rend.sharedMaterial = materials[(int)color.purple];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 0 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.green];
+            }
+            else if (colorCollected["blue"] == 0 && colorCollected["red"] == 1 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.orange];
+            }
+            else if (colorCollected["blue"] == 1 && colorCollected["red"] == 1 && colorCollected["yellow"] == 1)
+            {
+                rend.sharedMaterial = materials[(int)color.brown];
+            }
+            other.gameObject.SetActive(false);
+        }
+        else if (other.gameObject.CompareTag("Gate Trigger"))
+        {
+            if (colorCollected["blue"] == 1 && colorCollected["red"] == 0 && colorCollected["yellow"] == 1)
+            {
+                //rend.sharedMaterial = materials[(int)color.green];
+                output.text = "Win";
+            }
+            else
+            {
+                output.text = "lose";
+            }
+        }
     }
 }
