e85ddd4255120b3323060196a74f6a61a3d13510
Dorian Rosen <dorianrrosen@gmail.com>
2017-10-28 23:12:24 -0600

    update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     public GameObject player;
+    public Transform target;
 
     private Vector3 offset;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         offset = transform.position - player.transform.position;
-	}
-	
-	void LateUpdate () {
+    }
+
+    void LateUpdate()
+    {
+
+        transform.position = Vector3.Lerp(transform.position, target.position, 5.0f * Time.deltaTime) + offset; 
+
+        /*
         transform.position = player.transform.position + offset; 
-	}
+	
+    
+        transform.Rotate((transform.up * Input.GetAxisRaw("Mouse X")).normalized * Time.deltaTime);
+        */
+    }
 }
